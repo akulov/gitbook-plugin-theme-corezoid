75dae0d93684c0fc141b52e54c45269419109795
Akulov <andrey.akulov@gmail.com>
2016-09-20 18:44:06 +0300

    Update

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Gitbook custom theme for Corezoid docs
+# Тема офоромления Gitbook к документации облачной ОС Corezoid
+
+Это тема оформления в виде плагина для GitBook начиная с версии 3.0.0.
+
+Плагин добавляет новый блок с логотипом и списком языков, расширяя HTML шаблоны, CSS стили, JS скрипты базовой темы оформления [gitbook-plugin-theme-default](https://www.npmjs.com/package/gitbook-plugin-theme-default).
+
+Дополнительно сделан перевод заголовка результата поиска, отсутствующий в русской локализации (ru.json).
+
+Плагин можно использовать как шаблон-заготовку темы для GitBook.
+
+![Image](https://github.com/akulov/gitbook-plugin-theme-corezoid/blob/master/preview.png)
+
+## Установка
+
+Добавить плагин темы в файл конфигурации книги `book.json`:
+
+```js
+{
+    "plugins": [
+        "theme-corezoid"
+    ]
+}
+```
+
+И выполнить установку командой:
+
+``` bash
+gitbook install
+```
--- _assets/website/corezoid.js
+++ _assets/website/corezoid.js
@@ -1,17 +1,30 @@
-require(['gitbook', 'jquery'],function( gitbook, $){
+require(['gitbook', 'jquery'], function (gitbook, $) {
 
-    function resetContainerHeight(){
+    function setBookBodyHeight() {
         var gH = $(window).height();
         var hH = $('.header-inner').outerHeight();
         var $bookBody = $('.body-wrapper .book');
-        $bookBody.css( 'height', gH - hH );
+
+        $bookBody.css('height', gH - hH);
     }
 
-    gitbook.events.on('page.change',function(){
-        resetContainerHeight();
-    })
+    function setLangsUrl() {
+        var pageUrl = window.location.pathname.slice(3);
+        var $langsLinks = $('.langs li a');
 
-    $(window).on('resize',function(){
-        resetContainerHeight();
+        $langsLinks.each(function () {
+            var $lang = $(this).data('lang');
+
+            $(this).attr('href', '/' + $lang + pageUrl);
+        });
+    }
+
+    gitbook.events.on('page.change', function () {
+        setBookBodyHeight();
+        setLangsUrl();
+    });
+
+    $(window).on('resize', function () {
+        setBookBodyHeight();
     })
-})
+});
